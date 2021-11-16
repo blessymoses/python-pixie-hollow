bd5aef7633552327ff23aafe80c2733a38c4243d
blessymoses <blessymoses17@gmail.com>
2021-11-16 23:54:09 +0800

    20211116

--- fastAPI/fairy_stroke/fairy_stroke.py
+++ fastAPI/fairy_stroke/fairy_stroke.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseSettings
 
+# pydantic settings
+class Settings(BaseSettings):
+    message: str = "Default msg"
+
+
+settings = Settings()
 app = FastAPI()
 
 
 @app.get("/")
 async def home():
     return {"Howdy": "Fairy"}
+
+
+@app.get("/settings")
+def get_settings():
+    return {"message": settings.message}
